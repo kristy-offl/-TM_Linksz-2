0b100594fca430d4553bfb1c732b325bab2a2827
karthikr0910-dev <83643014+karthikr0910-dev@users.noreply.github.com>
2021-05-07 10:12:19 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -185,7 +185,7 @@ async def auto_filter (bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)}.                              <i>Must Join Our Channel For Enjoy Our Service by clicking this <b>@colorkannadi_LinkzZ</b>.          ⬇️ YOUR RESULTS ARE HERE ⬇️</i>:  <code>{query}</code>",
+                text=f"Found {(len_results)}.                                   <i>Must Join Our Channel For Enjoy Our Service by clicking this <b>@colorkannadi_LinkzZ</b>.          ⬇️ YOUR RESULTS ARE HERE ⬇️</i>:  <code>{query}</code>",
  
                 reply_markup=reply_markup,
                 parse_mode="html",
