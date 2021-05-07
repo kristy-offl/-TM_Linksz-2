21282feae6bcccfcb335cfe9bd9b6d61ef17ac96
karthikr0910-dev <83643014+karthikr0910-dev@users.noreply.github.com>
2021-05-07 09:38:55 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -146,10 +146,7 @@ async def callback_data(bot, update: CallbackQuery):
         
         reply_markup = InlineKeyboardMarkup(temp_results)
         
-        text=f"<i>Found</i> <code>{leng}</code>
-<i>Powered by: 🌀 @colorkannadi_LinkzZ 🌀.
-⬇️ YOUR RESULTS ARE HERE ⬇️:</i>
-<code>{query}</code>"
+        text=f"<i>Found</i> <code>{leng}</code> <i>Must Join Our Channel For Enjoy Our Service by clicking this @colorkannadi_LinkzZ.⬇️ YOUR RESULTS ARE HERE ⬇️:/i> <code>{query}</code>"
             
         try:
             await update.message.edit(
